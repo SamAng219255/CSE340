f50a136cc34dd127a317e6eb85812dbeeecd84d5
SamAng219255 <SamAng033146085@gmail.com>
2025-11-12 21:33:51 -0800

    Completed Week 03 Learning Activity 1.

---
+++ controllers/baseController.js
@@ -0,0 +1,9 @@
+const utilities = require("../utilities/")
+const baseController = {}
+
+baseController.buildHome = async function(req, res){
+  const nav = await utilities.getNav()
+  res.render("index", {title: "Home", pageStyle: "home", nav})
+}
+
+module.exports = baseController
